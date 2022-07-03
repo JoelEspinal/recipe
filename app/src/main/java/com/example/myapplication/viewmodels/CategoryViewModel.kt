32ffe5e93f7cb4adfccc435bec604bbe705472b7
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-07-02 20:34:55 -0400

    Rename categories fragment

---
+++ app/src/main/java/com/example/myapplication/viewmodels/CategoryViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.repositories.CategoryRepository
+
+class CategoryViewModel(private val repository: CategoryRepository): ViewModel(){
+
+}
