225fcba81aa49599eb8db22658e0af39d4d6bb74
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-08-10 15:03:42 -0400

    Move code to onViewCreated

--- app/src/main/java/com/example/myapplication/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/CategoriesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
@@ -13,37 +14,31 @@ import com.example.myapplication.repositories.CategoryRepository
 import com.example.myapplication.viewmodels.CategoriesViewModel
 
 class CategoriesFragment : Fragment() {
+    private val viewModel = CategoriesViewModel(CategoryRepository())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val viewModel = CategoriesViewModel(CategoryRepository())
-        val layout = inflater.inflate(R.layout.fragment_first, container, false)
-        val recyclerView = layout.findViewById<RecyclerView>(R.id.recycler_view_categories)
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_categories)
         val adapter = CategoryAdapter(viewModel)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(activity, 2)
 
-//        activity?.let {
-//            viewModel.categories().observe(it){ category ->
-//                category.let {
-//                    adapter.submitList(category)
-//                }
-//            }
-//        }
-
-        viewModel.categories().observe(activity!!) { category ->
-            category.let {
-                    adapter.submitList(category)
-                }
+        activity?.let {
+            viewModel.categories().observe(it) {
+                adapter.submitList(it)
+            }
         }
 
-        return layout
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
