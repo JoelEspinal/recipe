f079c8447269f04e4f5f530484d767f51f32a395
JoelEspinal <j.espinalcarrasco@gmail.com>
2022-08-10 15:02:46 -0400

    Fix issue with service call

--- app/src/main/java/com/example/myapplication/repositories/CategoryRepository.kt
+++ app/src/main/java/com/example/myapplication/repositories/CategoryRepository.kt
@@ -4,6 +4,8 @@ import android.util.Log
 import com.example.myapplication.data.remote.recipeService
 import com.example.myapplication.data.remote.responses.CategoriesResponse
 import com.example.myapplication.models.Category
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,28 +13,13 @@ import retrofit2.Response
 
 class CategoryRepository {
 
-    fun categories(): List<Category> {
-        var categories = listOf<Category>()
-
-       val call = recipeService.getCategories()
-
-        call.enqueue(
-            object : Callback<CategoriesResponse> {
-                override fun onResponse(
-                    call: Call<CategoriesResponse>,
-                    response: Response<CategoriesResponse>
-                ) {
-                    response.body()?.let { categories = it?.categories!! }
-
-                }
-
-                override fun onFailure(call: Call<CategoriesResponse>, t: Throwable) {
-                    TODO("Not yet implemented")
-                }
-
-            }
-        )
-
-        return categories
+    fun categories() : List<Category> {
+        val response = recipeService.getCategories().execute()
+        return if (response.isSuccessful) {
+            val body = response.body()!!
+            body.categories
+        } else {
+            listOf()
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/viewmodels/CategoriesViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/CategoriesViewModel.kt
@@ -1,21 +1,24 @@
 package com.example.myapplication.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myapplication.models.Category
 import com.example.myapplication.repositories.CategoryRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class CategoriesViewModel(private val repository: CategoryRepository): ViewModel(){
 
-    fun categories(): MutableLiveData<List<Category?>> {
-        val liveData =  MutableLiveData<List<Category?>>()
-//        viewModelScope.launch(Dispatchers.Main) {
-            liveData.value = repository.categories()
-//        }
+    private val liveData =  MutableLiveData<List<Category?>>()
 
-        return liveData
+    fun categories() : MutableLiveData<List<Category?>> {
+        CoroutineScope(Dispatchers.IO).launch {
+            liveData.postValue(repository.categories())
+        }
+
+        return liveData;
     }
 }
